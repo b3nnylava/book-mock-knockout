b190031f3679afa35063baf2bbbed5589f709b8b
b3nnylava <b3nnylava@horsefucker.org>
2018-10-17 19:02:46 -0500

    add icon/title, add css file, add toggles

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 jQuery(function($){
+	$('#view-toggle').click(function () {
+		if ($(this).text() == 'Card') {
+			$('#card-container').hide();
+			$('#table-container').show();
+			$(this).text('Grid');
+		} else {
+			$('#table-container').hide();
+			$('#card-container').show();
+			$(this).text('Card');
+		}
+	});
+
+	$('#theme-toggle').click(function () {
+		if ($(this).text() == 'Dark') {
+			$(this).text('Light');
+		} else {
+			$(this).text('Dark');
+		}
+	});
+
 	$('.table').footable({
 		toggleColumn: "last",
 		columns: [
